c721886fd637f1aa96a05c7030a0874c4b16bf7b
Kishor <124120029+moltresIn@users.noreply.github.com>
2024-08-14 10:19:37 +0530

    Revert "New updated code is pushed after updating the interface"

---
+++ lib/presentation/database_screen/widgets/viewhierarchy_item_widget.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/app_export.dart'; // ignore: must_be_immutable
+
+class ViewhierarchyItemWidget extends StatelessWidget {
+  const ViewhierarchyItemWidget({Key? key})
+      : super(
+          key: key,
+        );
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 106.h,
+      child: Align(
+        alignment: Alignment.centerRight,
+        child: SizedBox(
+          height: 110.v,
+          width: 95.h,
+          child: Stack(
+            alignment: Alignment.bottomRight,
+            children: [
+              CustomImageView(
+                imagePath: ImageConstant.imgImage,
+                height: 80.v,
+                width: 90.h,
+                alignment: Alignment.topCenter,
+              ),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    left: 23.h,
+                    right: 9.h,
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CustomImageView(
+                        imagePath: ImageConstant.imgImage2,
+                        height: 30.adaptSize,
+                        width: 30.adaptSize,
+                        alignment: Alignment.centerRight,
+                      ),
+                      Text(
+                        "1:25 PM",
+                        style: theme.textTheme.titleSmall,
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
